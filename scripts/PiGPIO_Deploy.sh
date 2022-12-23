f7e049604176296db57054a48222d303e169592f
Cesar Javaroni <filhc@amazon.com>
2022-12-23 14:37:17 -0800

    Adding component to execute PiGPIO daemon

---
+++ scripts/PiGPIO_Deploy.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+sudo /greengrass/v2/bin/greengrass-cli deployment create \
+--recipeDir ~/Documents/gaudanon-starter-project/recipes \
+--artifactDir ~/Documents/gaudanon-starter-project/artifacts \
+--merge "com.gaudanon.PiGPIO=1.0.0"
