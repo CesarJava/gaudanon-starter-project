95fc83467958ffcf27ce745e7b4d65e40f683f6c
Cesar Javaroni <filhc@amazon.com>
2022-12-23 16:16:49 -0800

    Correct starting up Daemon and versioning of the components

--- scripts/PiGPIO_Deploy.sh
+++ scripts/PiGPIO_Deploy.sh
@@ -3,4 +3,4 @@
 sudo /greengrass/v2/bin/greengrass-cli deployment create \
 --recipeDir ~/Documents/gaudanon-starter-project/recipes \
 --artifactDir ~/Documents/gaudanon-starter-project/artifacts \
---merge "com.gaudanon.PiGPIO=1.0.0"
+--merge "com.gaudanon.PiGPIO=1.0.1"
